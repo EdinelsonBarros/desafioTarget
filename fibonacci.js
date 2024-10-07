208a6cfed17745903ce1638314a04f05e13644b2
EdinelsonBarros <edinelsonlimabarros@gmail.com>
2024-10-07 13:56:39 -0300

    Segunda solução para teste de Fibonacci

--- fibonacci.js
+++ fibonacci.js
@@ -20,3 +20,36 @@ function fibcontains(){
 
 fibcontains()
 
+/*
+      Fiz a primeira resolução para o problema, mas fiquei martelando na cabeça que tinha uma forma melhor de resolver este problema, na qual qualquer número pudesse ser incluído e decomposto pela própria sequência de Fibonacci até 0 se fosse da sequência ou até um número menor do que zero se não fosse.
+*/
+
+
+let fiboResult2;
+let results = 10;
+let num2 = 4182;
+fibonums2 = [0, 1]
+
+function fibcontains(){
+            num - 1
+            for(let i = 0; results > 0; ){
+                  
+                  fiboResult = fibonums[i] + fibonums[i+1]
+                  fibonums.push(fiboResult)
+                  i++
+                  results = num - fibonums[i+1]
+                  console.log(results)
+                  console.log(fibonums)
+            }
+
+
+
+            if (results == 0){
+                  console.log(`O número: ${num} pertence a sequência de Fibonacci.`)
+            } else {
+                  console.log(`O número: ${num} não pertence a sequência de Fibonacci.`)
+            }
+      }
+
+fibcontains()
+
